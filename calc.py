7666e26f3db304fec982596229a60f4d8f375274
denis-beurive <denis.beurive@gmail.com>
2021-03-09 23:54:05 +0100

    update

--- calc.py
+++ calc.py
@@ -12,10 +12,10 @@ def ip2int(in_ip: str) -> Tuple[int, int, int, int, int]:
 
 def split_cidr(in_cidr: str) -> Optional[Tuple[str, str]]:
     p: Pattern = re.compile('^([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,'
-                            '3})/([0-9]{1,2})')
+                            '3})/([0-9]{1,2})$')
     m: Optional[Match] = p.match(in_cidr)
     if m is None:
-        return None, None
+        return None
     return m.group(1), m.group(2)
 
 
@@ -35,32 +35,43 @@ def int2ip_bin(in_int: int) -> str:
     return f"{n3:08b}.{n2:08b}.{n1:08b}.{n0:08b}"
 
 
-if len(sys.argv) != 2:
-    print("Usage: ip <cidr>")
+def print_cidr(in_cidr: str):
+    ip_net = split_cidr(in_cidr)
+    if ip_net is None:
+        raise Exception(f"Invalid CIDR \"{cidr:s}\"\n")
+
+    ip: str = ip_net[0]
+    net_len = int(ip_net[1])
+    ip_num, n3, n2, n1, n0 = ip2int(ip)
+    net_mask = (0xFFFFFFFF << (32 - net_len)) & 0xFFFFFFFF
+    host_mask = net_mask ^ 0xFFFFFFFF
+    net_part = ip_num & net_mask
+
+    print(f"ip={ip:s} len={net_len:d}\n")
+    print(f'network mask: 0x{net_mask:08X} ({int2ip_bin(net_mask):s}) '
+          f'{int2ip(net_mask):s}')
+    print(f'host mask:    0x{host_mask:08X} ({int2ip_bin(host_mask):s})')
+    print(f'network part: 0x{net_part:08X} ({int2ip(net_part):s})')
+    print(f'IP count:     {host_mask + 1:d}')
+    print(f'min IP:       {int2ip(net_part):s}')
+    print(f'max IP:       {int2ip(net_part | host_mask):s}')
+
+try:
+    if len(sys.argv) == 1:
+        cidrs: List[str] = ['10.0.0.0/8', '172.16.0.0/12', '192.168.0.0/16']
+        for cidr in cidrs:
+            print_cidr(cidr)
+            print("")
+        sys.exit(0)
+
+    if len(sys.argv) == 2:
+        cidr: str = sys.argv[1]
+        print_cidr(cidr)
+        sys.exit(0)
+
+    print("Usage: python calc.py [<cidr>]")
     sys.exit(1)
-
-cidr: str = sys.argv[1]
-ip_net = split_cidr(cidr)
-if ip_net is None:
-    print(f"Invalid CIDR \"{cidr:s}\"\n")
-    sys.exit(1)
-
-ip: str = ip_net[0]
-net_len = int(ip_net[1])
-ip_num, n3, n2, n1, n0 = ip2int(ip)
-net_mask = (0xFFFFFFFF << (32 - net_len)) & 0xFFFFFFFF
-host_mask = net_mask ^ 0xFFFFFFFF
-net_part = ip_num & net_mask
-host_part = ip_num & host_mask
-
-print(f"ip={ip:s} len={net_len:d}\n")
-print(f'network mask: 0x{net_mask:08X} ({int2ip_bin(net_mask):s}) '
-      f'{int2ip(net_mask):s}')
-print(f'host mask:    0x{host_mask:08X} ({int2ip_bin(host_mask):s})')
-print(f'network part: 0x{net_part:08X} ({int2ip(net_part):s})')
-print(f'IP count:     {host_mask+1:d}')
-print(f'min IP:       {int2ip(net_part):s}')
-print(f'max IP:       {int2ip(net_part | host_mask):s}')
-
+except Exception as e:
+    print(e)
 
 
